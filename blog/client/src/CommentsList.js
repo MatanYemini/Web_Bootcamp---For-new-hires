79ac6abf8eca5553874947af35e92fcf4215bdb8
Yemini <yeminimatan@gmail.com>
2020-04-28 10:27:48 +0300

    Update posts attributes

--- blog/client/src/CommentsList.js
+++ blog/client/src/CommentsList.js
@@ -1,22 +1,7 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import axios from 'axios';
 
-const CommentsList = ({ postId }) => {
-  const [comments, setComments] = useState([]);
-
-  const fetchData = useCallback(async () => {
-    const res = await axios.get(
-      `http://localhost:4001/posts/${postId}/comments`
-    );
-    console.log(res);
-    setComments(res.data);
-  }, [postId]);
-
-  useEffect(() => {
-    fetchData();
-  }, [fetchData]);
-
+const CommentsList = ({ comments }) => {
   const renderedComments = comments.map((comment) => {
     console.log(comment);
     return <li key={comment.id}>{comment.content}</li>;
--- blog/client/src/PostList.js
+++ blog/client/src/PostList.js
@@ -8,7 +8,7 @@ export const PostList = () => {
 
   useEffect(() => {
     async function fetchPosts() {
-      const res = await axios.get('http://localhost:4000/posts');
+      const res = await axios.get('http://localhost:4002/posts');
       setPosts(res.data);
       console.log(res.data);
     }
@@ -24,7 +24,7 @@ export const PostList = () => {
       >
         <div className='card-body'>
           <h3>{post.title}</h3>
-          <CommentsList postId={post.id} />
+          <CommentsList comments={post.comments} />
           <CommentCreate postId={post.id} />
         </div>
       </div>
