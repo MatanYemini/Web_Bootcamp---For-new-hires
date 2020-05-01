5a72f95f2a27629f3d10b07b118f3960bcaf744f
Yemini <yeminimatan@gmail.com>
2020-05-02 00:44:20 +0300

    Update k8s ingress-nginx configuration

--- blog/posts_service/index.js
+++ blog/posts_service/index.js
@@ -59,11 +59,11 @@ app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
+app.get('/posts/create', (req, res) => {
   res.send(posts);
 });
 
-app.post('/posts', async (req, res) => {
+app.post('/posts/create', async (req, res) => {
   const id = randomBytes(4).toString('hex');
   const { title } = req.body;
 
